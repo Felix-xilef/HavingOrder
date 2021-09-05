3a4ad3cce5c30b7bafaca9b5bd2c2d06d9803c93
Felix-xilef <petizfelix@gmail.com>
2021-09-04 21:34:07 -0300

    Creating Actionbar

--- app/src/main/java/com/fatec/havingorder/Orders.java
+++ app/src/main/java/com/fatec/havingorder/Orders.java
@@ -1,8 +1,10 @@
 package com.fatec.havingorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Orders extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Orders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+
+        Toolbar appToolBar = (Toolbar) findViewById(R.id.appToolbar);
+        setSupportActionBar(appToolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.nav_items, menu);
+
+        return true;
     }
 }
